b0f96e1c8754b95cce94636cb73bbacf40773caf
john <udsn2@sina.com>
2013-03-07 01:28:40 +0800

    Initial Commit

---
+++ findit/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  findit
+//
+//  Created by zuzu on 13-3-7.
+//  Copyright (c) 2013年 zuzu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
