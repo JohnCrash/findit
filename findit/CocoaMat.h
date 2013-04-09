b3daccbc842e0e6df88acad2031b8c7da1beebf6
John <udsn2@sina.com>
2013-04-10 05:47:01 +0800

    增加测试选项

---
+++ findit/CocoaMat.h
@@ -0,0 +1,39 @@
+//
+//  CocoaMat.h
+//  findit
+//
+//  Created by zuzu on 13-4-9.
+//  Copyright (c) 2013年 zuzu. All rights reserved.
+//
+
+#ifndef findit_CocoaMat_h
+#define findit_CocoaMat_h
+
+#import <Foundation/Foundation.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+
+/*
+    创建一个CGContextRef依据Mat的大小
+    m必须是一个CV_8UC4格式
+ */
+CGContextRef CreateBitmapContextFromMat(const Mat& m);
+
+/*
+    将img完整的复制到cgtx中
+ */
+void DrawImageToCGContext(CGImageRef img,CGContextRef cgtx,int width,int height);
+
+/*
+    创建一个宽度和高度为width,height的Mat,缩放img图像到Mat中
+ */
+Mat CreateMatFromCGImage(CGImageRef img,int width,int height);
+
+/*
+    根据m创建一个CGImage,注意m和该CGImage共享数据.
+ */
+CGImage* CreateCGImageFromMat(Mat& m);
+
+#endif
---
+++ findit/CocoaMat.mm
@@ -0,0 +1,93 @@
+//
+//  CocoaMat.mm
+//  findit
+//
+//  Created by zuzu on 13-4-9.
+//  Copyright (c) 2013年 zuzu. All rights reserved.
+//
+
+#include "CocoaMat.h"
+
+/*
+ 根据m创建一个CGContextRef
+ */
+CGContextRef CreateBitmapContextFromMat(const Mat& m)
+{
+    CGContextRef context = NULL;
+    CGColorSpaceRef colorSpace;
+    
+   // NSAssert(m.type()==CV_8UC4,@"Mat type must be CV_8UC4");
+    
+    colorSpace = CGColorSpaceCreateDeviceRGB();
+    if (colorSpace == NULL)
+    {
+        NSLog(@"Error allocating color space");
+        return NULL;
+    }
+    
+    context = CGBitmapContextCreate (m.data, m.cols,m.rows, 8, m.cols*m.elemSize(),
+                                     colorSpace, kCGImageAlphaPremultipliedLast);
+    if (context == NULL)
+    {
+        NSLog(@"Context not created!");
+    }
+    CGColorSpaceRelease( colorSpace );
+    return context;
+}
+
+/*
+ 将CGImageRef复制到cgtx
+ */
+void DrawImageToCGContext(CGImageRef img,CGContextRef cgtx,int width,int height)
+{
+    CGRect rect = {{0,0},{0,0}};
+    rect.size.width = width;
+    rect.size.height = height;
+    CGContextDrawImage(cgtx, rect, img);
+}
+
+Mat CreateMatFromCGImage(CGImageRef img,int width,int height)
+{
+    Mat m;
+    m.create(height,width,CV_8UC4);
+    CGContextRef ctx = CreateBitmapContextFromMat(m);
+    if( ctx )
+    {
+        DrawImageToCGContext(img, ctx,width,height);
+        CGContextRelease(ctx);
+    }
+    return m;
+}
+
+static void releaseMat(void *info, const void *data, size_t size)
+{
+    Mat* pm = (Mat*)info;
+    if( pm )delete pm;
+}
+
+CGImage* CreateCGImageFromMat(Mat& m)
+{
+    CGImage* img;
+    CGColorSpaceRef colorSpace;
+    colorSpace = CGColorSpaceCreateDeviceRGB();
+    if (colorSpace == NULL)
+    {
+        NSLog(@"Error allocating color space");
+        return NULL;
+    }
+    /*
+     增加Mat引用计数
+     */
+    Mat* bk = new Mat(m);
+    CGDataProviderRef providerRef = CGDataProviderCreateWithData(bk,
+                                                                 m.data,
+                                                                 m.total()*m.elemSize(),
+                                                                 releaseMat);
+    img = CGImageCreate(m.cols,m.rows,
+                        m.elemSize1()*8,m.elemSize()*8,m.step1(),
+                        colorSpace,kCGBitmapByteOrderDefault,providerRef,
+                        NULL,YES,kCGRenderingIntentDefault);
+    CGColorSpaceRelease( colorSpace );
+    CGDataProviderRelease(providerRef);
+    return img;
+}
